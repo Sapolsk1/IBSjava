2e96520ba58766643b5ef88a79c1e2825cdad8a2
Nursultan.Unbaev <Nursultan.Unbaev@x5.ru>
2021-09-18 20:06:48 +0300

    changes for hm4

--- src/main/java/HomeWork4.java
+++ src/main/java/HomeWork4.java
@@ -11,11 +11,11 @@ public class HomeWork4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите первое значение x = ");
+        System.out.print("Введите первое значение x = ");
         double x = scanner.nextDouble();
-        System.out.println("Введите первое значение y = ");
+        System.out.print("Введите первое значение y = ");
         double y = scanner.nextDouble();
-        System.out.println("Введите первое значение z = ");
+        System.out.print("Введите первое значение z = ");
         double z = scanner.nextDouble();
 
         double b = x + y + z;
