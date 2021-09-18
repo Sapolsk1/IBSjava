c86672f74c723bdf3c0e8dd075579a61b242c404
Nursultan.Unbaev <Nursultan.Unbaev@x5.ru>
2021-09-18 20:48:34 +0300

    add hm5

---
+++ src/main/java/HomeWork5.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class HomeWork5 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double number1 = getDob();
+        double number2 = getDob();
+        char operation = getOper();
+        double result = caclulate(number1, number2, operation);
+        System.out.println("Результат операции: " + Math.floor(result));
+    }
+
+    public static double getDob() {
+        System.out.println("Введите число:");
+        double num;
+        if (scanner.hasNextDouble()) {
+            num = scanner.nextDouble();
+        } else {
+            System.out.println("Ошибка ввода числа!");
+            scanner.next();
+            num = getDob();
+        }
+        return num;
+    }
+
+    public static char getOper() {
+        System.out.println("Введите операцию:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Ошибка ввода операции");
+            scanner.next();
+            operation = getOper();
+        }
+        return operation;
+
+    }
+
+    public static double caclulate(double num1, double num2, char op) {
+        double result;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Операция неверная.");
+                result = caclulate(num1, num2, getOper());
+        }
+        return result;
+    }
+}
