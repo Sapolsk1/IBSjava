2ab6ec879d645f884d82464eca608eb39f22d06d
Nursultan.Unbaev <Nursultan.Unbaev@x5.ru>
2021-09-20 21:43:57 +0300

    for future hm5

---
+++ src/main/java/test/test.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+//    Map<Character, Double> map = new HashMap<>();
+//        map.put('+', num1 + num2);
+//        map.put('-', num1 - num2);
+//        map.put('*', num1 * num2);
+//        map.put('/', num1 / num2);
+//        if (op == '/' && num2 == 0) {
+//        System.out.println("На ноль делить нельзя");
+//    } else {
+//        if (map.containsKey(op)) {
+//            System.out.println("Результат операции: " + map.get(op));
+//        }
+//    }
+}
